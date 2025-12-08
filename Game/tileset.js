cf328064107c09d4c618f984d7141219a312dcff
GoldenIShark <ahmadytta02@gmail.com>
2025-12-08 11:36:20 +0700

    Update tileset.js

--- Game/tileset.js
+++ Game/tileset.js
@@ -3,29 +3,30 @@ window.TILE_SIZE = 48;
 window.TILE_COLOR = {
   0: "#2e8b57", // rumput
   1: "#2a6bb5", // air
-  2: "#c2b280"  // pasir
+  2: "#c2b280", // pasir
+  3: "#3E2A1F", // lumpur
+  4: "#E8F6FF", // salju
+  5: "#009BFF"  // ice
 };
 
 window.walkable = {
   0: true,
   1: false,
-  2: true
+  2: true,
+  3: true,
+  4: true,
+  5: true
 };
 
-// tilemap akan dimuat dari map.txt
 window.tilemap = [];
 window.mapLoaded = false;
 
-// load map.txt
-fetch("map.txt")
-  .then(res => res.text())
-  .then(text => {
-    window.tilemap = text
-      .trim()
-      .split("\n")
-      .map(line => line.split(",").map(n => Number(n)));
-
-    window.mapLoaded = true;
-    console.log("Tilemap loaded:", window.tilemap);
+// ============ LOAD MAP JSON ============
+fetch("map.json")
+  .then(res => res.json())   // <-- yang benar untuk JSON
+  .then(data => {
+      window.tilemap = data;
+      window.mapLoaded = true;
+      console.log("Tilemap loaded:", data.length, "x", data[0].length);
   })
-  .catch(err => console.error("Failed to load map.txt:", err));
+  .catch(err => console.error("Failed to load map.json:", err));
