d0b03d56e69825ca6e9d74fcb1989f6e55e01bce
GoldenIShark <ahmadytta02@gmail.com>
2025-12-10 06:31:53 +0700

    Update tileset.js

--- Game/tileset.js
+++ Game/tileset.js
@@ -1,32 +1,18 @@
-window.TILE_SIZE = 48;
-
+// warna tile
 window.TILE_COLOR = {
-  0: "#2e8b57", // rumput
-  1: "#2a6bb5", // air
-  2: "#c2b280", // pasir
-  3: "#3E2A1F", // lumpur
-  4: "#E8F6FF", // salju
-  5: "#009BFF"  // ice
+    0:"#2e8b57", // rumput
+    1:"#2a6bb5", // air
+    2:"#c2b280", // pasir
+    3:"#3E2A1F", // lumpur
+    4:"#E8F6FF", // salju
+    5:"#009BFF"  // air beku
 };
 
 window.walkable = {
-  0: true,
-  1: false,
-  2: true,
-  3: true,
-  4: true,
-  5: true
+    0:true,
+    1:false,
+    2:true,
+    3:true,
+    4:true,
+    5:true
 };
-
-window.tilemap = [];
-window.mapLoaded = false;
-
-// ============ LOAD MAP JSON ============
-fetch("map.json")
-  .then(res => res.json())   // <-- yang benar untuk JSON
-  .then(data => {
-      window.tilemap = data;
-      window.mapLoaded = true;
-      console.log("Tilemap loaded:", data.length, "x", data[0].length);
-  })
-  .catch(err => console.error("Failed to load map.json:", err));
